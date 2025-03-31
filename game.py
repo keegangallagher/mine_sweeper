8fca7f5e8ab4dd5f4abf8befc7059157bac8b65d
keegangallagher <123406548+keegangallagher@users.noreply.github.com>
2025-03-31 12:29:37 -0500

    Added desc of gamboard and flag function

--- game.py
+++ game.py
@@ -6,12 +6,14 @@
 # Luke Nowolis(009)
 # Kaelen Bobber (009)
 # Keegan Gallagher (009)
-#
-#
-#This is a test hello test
-def game_board(size):
+#we have a seperate file that has a random list of bombs and numbers that is going to be attatched to this one to use in the boxes on the gameboard
+#we plan to have a list generated with all the bomb locations and then a second list which is whats been revealed and where the user wants flags
+
+def flag(): #this function is used to mark on the board where the user thinks bombs are
+  
+def game_board(size): #the gameboad will be a grid with numbers on the side and boxes with the numbers or flags inside
 
 
 def main():
-
+  gameboard() #calls gameboard
 main()
